c07667c4f4e61cdcc9db0e3dfb128c7d3fda57c6
Connor Fuhrman <connorfuhrman@email.arizona.edu>
2022-09-21 19:19:27 -0700

    Updates to main function

--- MathOpsExampleDeployment/Top/Main.cpp
+++ MathOpsExampleDeployment/Top/Main.cpp
@@ -1,46 +1,36 @@
+#include "BasicTypes.hpp"
 #include "MathOpsExampleDeployment/MathTypes/MathOpEnumAc.hpp"
 #include "MathOpsExampleTopologyDefs.hpp"
 #include "MathSender.hpp"
+#include "Task.hpp"
 #include <MathOpsExampleDeployment/Top/MathOpsExampleTopologyAc.hpp>
 
 #include <csignal>
 #include <iostream>
+#include <thread>
+#include <functional>
+#include <random>
+
 
 MathOpsExample::TopologyState state;
 
 volatile sig_atomic_t running = 1;
+
+
 
 static void sighandler(int)
 {
   std::cout << "\nGot end signal. Shutting down ..." << "\n";
   MathOpsExample::teardown(state);
   running = 0;
+
 }
 
-namespace MathOpsExample {
-  struct MathOpPortPkg {
-    F32 rhs, lhs;
-    MathOp op;
-  };
-}
-
-static inline auto get_random_mathop()
+static void blockDrv_th_entrypoint(const volatile sig_atomic_t& running,
+				   std::function<void()> do_cycle)
 {
-  return MathOpsExample::MathOpPortPkg{1, 2, MathOpsExample::MathOp::ADD};
+  while(running) do_cycle();
 }
-
-static inline void do_cycle()
-{
-  auto op_to_invoke = get_random_mathop();
-  MathOpsExample::mathSender.do_mathOpReq(0,
-					  op_to_invoke.lhs,
-					  op_to_invoke.op,
-					  op_to_invoke.rhs);
-  
-  MathOpsExample::blockDrv.callIsr();
-  Os::Task::delay(5000); // delay for 5s
-}
-
 
 
 int main()
@@ -53,7 +43,47 @@ int main()
   std::signal(SIGINT, sighandler);
   std::signal(SIGTERM, sighandler);
 
-  while (running) do_cycle();
+  // Launch thread to call the block driver
+  // manually so the MathReceiver will flush
+  // its queue
+  std::jthread blockDrv_th(blockDrv_th_entrypoint,
+			  std::ref(running),
+			  [] () {
+			    MathOpsExample::blockDrv.callIsr();
+			    Os::Task::delay(500);
+			  });
+
+
+  // While running give random invocations of the
+  // MathSender's output MathOpsReqOut port
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<unsigned int> op_distrib(0, 3);
+  std::uniform_real_distribution<F32> val_distrib(1, 100);
+  const auto make_random_op = [&op_distrib, &gen]() {
+    return static_cast<MathOpsExample::MathOp::t>(op_distrib(gen));
+  };
+
+  const auto print_req = [] (auto lhs, auto op, auto rhs) {
+    std::string op_str;
+    switch(op) {
+    case MathOpsExample::MathOp::ADD: op_str = "+"; break;
+    case MathOpsExample::MathOp::SUB: op_str = "-"; break;
+    case MathOpsExample::MathOp::DIV: op_str = "/"; break;
+    case MathOpsExample::MathOp::MUL: op_str = "*"; break;
+    }
+    std::cout << "Requesting " << lhs << " " << op_str << " " << rhs << "\n";
+  };
+  
+  while (running)
+  {
+    auto op = make_random_op();
+    auto lhs = val_distrib(gen);
+    auto rhs = val_distrib(gen);
+    print_req(lhs, op, rhs);
+    MathOpsExample::mathSender.do_mathOpReq(0, lhs, op, rhs);
+    Os::Task::delay(10000);
+  }
 
   std::cout << "Exiting!!" << "\n";
   
